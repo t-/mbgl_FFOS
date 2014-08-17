5cccd46798e99938ff155dcbe42f679e73c086c3
ti <t.m.d.graen@googlemail.com>
2014-08-17 08:19:23 +0200

    edited build script, spawns debug server

--- build.sh
+++ build.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
-../node_modules/ionic/bin/ionic platform add firefoxos
-../node_modules/ionic/bin/ionic build firefoxos
-#../node_modules/ionic/bin/ionic emulate firefoxos
+#sudo npm install -g cordova ionic
 
-#../node_modules/ionic/bin/ionic platform add ubuntu
-#../node_modules/ionic/bin/ionic build ubuntu
-#../node_modules/ionic/bin/ionic emulate ubuntu
+#for deploy
+ionic platform add firefoxos
+ionic build firefoxos
+
+#for dev
+ionic serve # local web server 
 
 
-~                                                   
+
